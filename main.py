55eff5c97b8898d911b23dc467d07055ee2f9a13
Krisna31 <krisnaaaputraaa@gmail.com>
2024-05-26 14:31:48 +0700

    FIX ~ default input change to heksakosioiheksekontaheksafobia

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ def run_simulation(num_simulations, str1, str2):
 def main():
     st.title("Anagram Check Simulation")
     num_simulations = st.number_input("Number of Simulations:", min_value=1, step=1, value=1000)
-    str1 = st.text_input("Input String 1:", value="listen")
-    str2 = st.text_input("Input String 2:", value="silent")
+    str1 = st.text_input("Input String 1:", value="heksakosioiheksekontaheksafobia")
+    str2 = st.text_input("Input String 2:", value="heksakosioiheksekontaheksafobia")
     run_simulation_button = st.button("Run Simulation")
 
     if not str1.strip() or not str2.strip():
